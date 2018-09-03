56dab3852966abe31a32c7ba5b7b342fa5e94ab7
Ivan Perun <peruni101@gmail.com>
2018-09-03 15:40:13 -0400

    add items

--- js/toDo.js
+++ js/toDo.js
@@ -1,12 +1,21 @@
 // check off todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 // click on x to delete
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+// add new items to List
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var toDoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + toDoText + "</li>");
+  }
 });

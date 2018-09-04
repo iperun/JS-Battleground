4f3c5f96f1dd40044875699b39399a829ad61778
Ivan Perun <peruni101@gmail.com>
2018-09-03 21:22:28 -0400

    style finish

--- js/toDo.js
+++ js/toDo.js
@@ -16,6 +16,10 @@ $("input[type='text']").keypress(function(event) {
   if (event.which === 13) {
     var toDoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + toDoText + "</li>");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + toDoText + "</li>");
   }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
 });

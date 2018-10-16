78e973cce937fdfa30361a825d4d2a57993c7e6d
Ivan Perun <peruni101@gmail.com>
2018-10-15 22:05:01 -0400

    styling

--- html/js/webcamFun.js
+++ html/js/webcamFun.js
@@ -1,1 +1,5 @@
-alert ('connected');
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
